36f450d33d15541e69adcd72623bfc73ac21034f
begeles2000 <begeles2000@gmail.com>
2015-10-06 18:28:55 +0200

    añadido los codigos iniciales

---
+++ raspi_mon_sys/aemqtt.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Copyright (C) 2015 Miguel Lorenzo
+# Use of this source code is governed by the MIT license found in the LICENSE file.
+
+#recoge la prediccion de temperatura de AEMET para los proximos 7 dias y la publica mediante MQTT
+
+
+import Aemet
+import time
+import paho.mqtt.client as paho
+
+def on_connect(pahoClient, obj, rc):
+# Once connected, publish message
+        print "Connected Code = %d"%(rc)
+        client.publish("casa/prediccion/{0}/temperatura/max".format(tiempo.get_localidad()), tiempo.get_temperatura_maxima(), 0)
+        client.publish("casa/prediccion/{0}/temperatura/min".format(tiempo.get_localidad()), tiempo.get_temperatura_minima(), 0)
+        
+
+
+
+
+host="192.168.1.104"
+port=1883
+
+dia = time.strftime("%d/%m/%Y")
+localidad = "46190"
+tiempo = Aemet.Localidad(localidad, dia)
+
+#print 'Localidad: ', tiempo.get_localidad()
+#print 'Temp. max:', tiempo.get_temperatura_maxima()
+#print 'Temp. min:', tiempo.get_temperatura_minima()
+
+client=paho.Client()
+client.connect(host, port, 60)
+client.loop_start()
+
+while True:
+    try:
+        client.publish("casa/prediccion/{0}/{1}/temperatura/max".format(tiempo.get_localidad(),time.strftime("%d-%m-%Y")), tiempo.get_temperatura_maxima(), 0)
+        client.publish("casa/prediccion/{0}/{1}/temperatura/min".format(tiempo.get_localidad(),time.strftime("%d-%m-%Y")), tiempo.get_temperatura_minima(), 0)
+        for datohora in tiempo.get_temperatura_horas():
+            client.publish("casa/prediccion/{0}/{1}/temperatura/hora/{2}".format(tiempo.get_localidad(),time.strftime("%d-%m-%Y"),datohora[0]),datohora[1], 0)
+
+    except ValueError:
+        client.publish("casa/{0}".format(mac),"error")
+    time.sleep(3600)
---
+++ raspi_mon_sys/plugemoncon.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2015 Miguel Lorenzo
+# Use of this source code is governed by the MIT license found in the LICENSE file.
+
+#Publica las lecturas de los diferentes Circles de plugwise mediante MQTT en un topic
+#adecuado al emonhub para que se muestre en el emonCMS
+
+
+
+import time
+import paho.mqtt.client as mqtt
+from plugwise import *
+import plugwise.util
+
+
+def on_connect(client, userdata, rc):
+    #print("Connected with result code "+str(rc))
+    client.subscribe("emonhub/#")
+
+def on_message(client, userdata, msg):
+	#print(msg.topic+" "+str(msg.payload))
+
+#Configuracion de la conexion al broker MQTT
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("192.168.1.102", 1883, 60)
+
+#Configuracion de la conexion con los plugwise
+DEFAULT_SERIAL_PORT = "/dev/ttyUSB0" #Se define el puerto del usb
+macs = [["000D6F00029C19F9",3],["000D6F0003562CBD",4]] #listado de macs de los circles que tenga que leer y su Id de nodo asiganda en el emon
+device = DEFAULT_SERIAL_PORT
+device = Stick(device)
+
+client.loop_start()
+
+while True:
+    for mac in macs:
+        c = Circle(mac[0], device)
+        try:
+            client.publish("emonhub/rx/{0}/values".format(mac[1]),"{0:.2f},{1}".format(c.get_power_usage(),c.get_info()['relay_state']))
+            #time.sleep(1)
+        except ValueError:
+            client.publish("dev/errors/{0}".format(mac),"error")
+    #client.loop(timeout=5)
+    time.sleep(5)
