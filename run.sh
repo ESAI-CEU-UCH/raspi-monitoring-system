b699f240c4486e47dc0339f344412f0f5f6d6397
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-11 11:07:07 +0100

    Replaced runuser by su command

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $HOME/raspi-monitoring-system
+export PYTHONPATH=$(pwd)
 
 screen -d -m python raspi_mon_sys/MailLoggerServer.py /etc/mail_credentials.json
 screen -d -m python raspi_mon_sys/MainMonitoringSystem.py
--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -2,4 +2,4 @@
 # This script requires SSH keys properly configured. It has been prepared to be
 # executed at startup as a system daemon. This tunnel allow to connect to mongodb
 # in esainet01 machine.
-runuser -l ssh_tunnel -c 'autossh -f -N -C -L 27018:localhost:27018 ssh_tunnel@esainet01'
+su ssh_tunnel -c 'autossh -f -N -C -L 27018:localhost:27018 ssh_tunnel@esainet01'
