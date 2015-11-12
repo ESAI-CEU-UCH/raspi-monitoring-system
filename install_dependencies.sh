e61d7d19ea4243dbed7258fe22be4e068120c36a
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-12 12:28:55 +0100

    Fixed typo :(

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,4 @@
 # Executes installation of needed dependencies.
 # pip install uuid
 pip install enum paho-mqtt
-aptitude install python-zmq screen autossh mongodb-clients libmongo-client0python-pymongo
+aptitude install python-zmq screen autossh mongodb-clients libmongo-client0 python-pymongo
