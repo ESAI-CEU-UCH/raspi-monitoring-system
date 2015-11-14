690810883073b852e85fea40efe0712a1bb3d625
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-14 11:29:34 +0100

    Updated prepare.sh

--- open_energy_monitor/prepare.sh
+++ open_energy_monitor/prepare.sh
@@ -3,6 +3,5 @@ if [[ ! -e rpi-serial-console ]]; then
     wget https://raw.github.com/lurch/rpi-serial-console/master/rpi-serial-console
     chmod +x rpi-serial-console
 fi
-sudo rpi-serial-console disable
-echo "This software needs to reboot raspimon"
-sudo reboot
+sudo ./rpi-serial-console disable
+echo "Remember, this software needs to reboot raspimon"
