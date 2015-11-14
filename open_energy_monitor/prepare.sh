b8d58e9ee028ac771d4f61d74e7440f2ff186363
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-14 12:05:41 +0100

    Adapted for systemd

--- open_energy_monitor/prepare.sh
+++ open_energy_monitor/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [[ ! -e rpi-serial-console ]]; then
-    wget https://raw.github.com/lurch/rpi-serial-console/master/rpi-serial-console
-    chmod +x rpi-serial-console
-fi
+# if [[ ! -e rpi-serial-console ]]; then
+#     wget https://raw.github.com/lurch/rpi-serial-console/master/rpi-serial-console
+#     chmod +x rpi-serial-console
+# fi
 sudo ./rpi-serial-console disable
 echo "Remember, this software needs to reboot raspimon"
