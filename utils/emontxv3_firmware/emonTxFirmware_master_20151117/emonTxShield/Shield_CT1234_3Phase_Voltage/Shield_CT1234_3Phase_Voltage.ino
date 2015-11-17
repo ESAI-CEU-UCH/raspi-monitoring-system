ce4da53ab8aedefe809d87a7241c460d9eb55cf3
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-17 15:09:55 +0100

    Adding firmware sources

---
+++ utils/emontxv3_firmware/emonTxFirmware_master_20151117/emonTxShield/Shield_CT1234_3Phase_Voltage/Shield_CT1234_3Phase_Voltage.ino
@@ -0,0 +1,1 @@
+See emonTx V3.4 RFM 3-phase example and make the changes detailed in the comments to use with emonTx Shield
---
+++ utils/emontxv3_firmware/emonTxFirmware_master_20151117/emonTxV3/RFM/emonTxV3.2/emonTxV3_2_3Phase_Voltage/emonTxV3_2_3Phase_Voltage.ino
@@ -0,0 +1,1 @@
+see emonTx V3.4 three phase example and make the changes noted in the comments to work on emonTx V3.2
