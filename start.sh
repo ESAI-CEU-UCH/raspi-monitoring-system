445af50f97fe448a40cb25d1220f6ba3cb007aa5
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-19 09:11:50 +0100

    Minor change

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ screen -d -m python raspi_mon_sys/MailLoggerServer.py /etc/mail_credentials.json
 screen -d -m python raspi_mon_sys/MainMonitoringSystem.py
 
 # For systemd startup script.
-while 1; do sleep 3600; done
+while true; do sleep 3600; done
