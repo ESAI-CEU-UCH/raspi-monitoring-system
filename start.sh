5dee9fff2e6a45df7c6230c2b22b9f92bd01ce3c
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-19 09:10:42 +0100

    Minor change

--- start.sh
+++ start.sh
@@ -4,3 +4,6 @@ export PYTHONPATH=$(pwd)
 
 screen -d -m python raspi_mon_sys/MailLoggerServer.py /etc/mail_credentials.json
 screen -d -m python raspi_mon_sys/MainMonitoringSystem.py
+
+# For systemd startup script.
+while 1; do sleep 3600; done
