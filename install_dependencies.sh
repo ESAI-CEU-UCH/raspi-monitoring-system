3fd63a0a62cf620561b6935acdd510d3f179b888
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-30 20:24:23 +0100

    Updated dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Executes installation of needed dependencies.
 # pip install uuid
-pip install enum paho-mqtt pytz
+# influxdb is available under raspbian apt, but it is an old version :(
+pip install enum paho-mqtt pytz influxdb
 aptitude install python-zmq screen autossh mongodb-clients libmongo-client0 python-pymongo minicom python-crcmod python-serial python-lxml nodejs nodejs-legacy npm
