038f41587ca05b3a2423ad1831a5c096a486e440
Paco Zamora Martinez <pakozm@gmail.com>
2015-11-30 21:17:44 +0100

    minor generalizations

--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This script requires SSH keys properly configured. It has been prepared to be
 # executed at startup as a system daemon. This tunnel allow to connect to mongodb
-# in esainet01 machine.
-su ssh_tunnel -c 'autossh -f -N -C -L 27018:localhost:27018 ssh_tunnel@esainet01'
+# in raspimondbserver machine.
+su ssh_tunnel -c 'autossh -f -N -C -L 27018:localhost:27018 ssh_tunnel@raspimondbserver'
--- utils/database_manager/execute.sh
+++ utils/database_manager/execute.sh
@@ -2,10 +2,11 @@
 if [[ ! -e node_modules ]]; then
     npm install mongo-express
 fi
-echo "Warning!!! Requires SSH tunnel to esainet01 27018 port"
+echo "Warning!!! Requires SSH tunnel to raspimondbserver 27018 port"
 OLDPWD=$(pwd)
 cd node_modules/mongo-express/
 node app.js &
+sleep 5
 gnome-open http://localhost:8081
 wait
 cd $OLDPWD
